7927ec3d54d8d5b631a00fade003384abd864240
Ed Summers <ehs@pobox.com>
2019-05-14 17:29:01 +0000

    the perl script was not cutting it

--- stats.py
+++ stats.py
@@ -9,27 +9,20 @@ import sys
 from collections import Counter
 
 nicks = Counter()
-coffees = Counter()
+drinks = Counter()
 
 for line in sys.stdin:
-    m = re.search(r'zoia brews and pours a cup of (.+), and sends it sliding down the bar to (\w+) \(?', line)
+    m = re.search(r'zoia brews and pours a (pot|cup) of (.+), and sends it sliding down the bar to (\w+)( \()?', line)
     if m:
-        nicks[m.group(1)] += 1
-        coffees[m.group(2)] += 1
-        
-with open('nicks.csv', 'w') as fh:
-    out = csv.writer(fh)
-    out.writerow(['nick', 'coffees'])
-    for nick, coffee in nicks.most_common():
-        out.writerow([nick, coffee])
+        nicks[m.group(3)] += 1
+        drinks[m.group(2)] += 1
 
-with open('coffees.csv', 'w') as fh:
-    out = csv.writer(fh)
-    out.writerow(['coffee', 'served'])
-    for coffee, served in coffees.most_common():
-        out.writerow([coffee, served])
+def write_csv(filename, counter, headings):
+    with open(filename, 'w') as fh:
+        out = csv.writer(fh)
+        out.writerow(headings)
+        for label, count in counter.most_common():
+            out.writerow([label, count])
 
-
-
-
-
+write_csv("nicks.csv", nicks, ['nick', 'drinks'])
+write_csv("drinks.csv", drinks, ['drink', 'count'])
